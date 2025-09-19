c21929a67cc474b006b533dfc54e58b1ce5125eb
Arda Kilicdagi <ardakilicdagi@gmail.com>
2025-09-20 00:14:00 +0400

    chore: linter

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -15,7 +15,7 @@ module.exports = {
             presets: [
               '@babel/preset-env',
               ['@babel/preset-react', { runtime: 'automatic' }],
-              '@babel/preset-typescript'
+              '@babel/preset-typescript',
             ],
           },
         },
